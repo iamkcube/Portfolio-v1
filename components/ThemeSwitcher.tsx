dce91753dbe744067a96bf6a8af4a4723d6f869f
Kalinga Kumar Khatua <kalingakhatua@gmail.com>
2024-11-06 21:35:32 +0530

    Theme switch accessible

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -31,7 +31,7 @@ const ThemeSwitcher = () => {
 	return (
 		<button
 			id="theme-switcher"
-			aria-labelledby="Theme Switcher"
+			aria-label="Theme Switch"
 			className="cursor-pointer bg-transparent border-0 opacity-80 w-8 h-8 rounded-full relative isolation-isolate hover:opacity-100 focus-visible:opacity-100"
 			onClick={toggleTheme}
 		>
